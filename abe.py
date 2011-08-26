9083bd493e3dfba23e7622a77299af1b2b465422
John Tobey <John.Tobey@gmail.com>
2011-08-26 16:41:48 -0400

    Stripped trailing whitespace from source code.

--- abe.py
+++ abe.py
@@ -5,12 +5,12 @@
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public
 # License along with this program.  If not, see
 # <http://www.gnu.org/licenses/agpl.html>.
@@ -770,7 +770,7 @@ class Abe:
 
         # XXX Unneeded outer join.
         in_rows = map(parse_row, abe.store.selectall("""
-            SELECT 
+            SELECT
                 txin.txin_pos,
                 txin.txin_scriptSig,
                 txout.txout_value,
@@ -788,7 +788,7 @@ class Abe:
 
         # XXX Only two outer JOINs needed.
         out_rows = map(parse_row, abe.store.selectall("""
-            SELECT 
+            SELECT
                 txout.txout_pos,
                 txout.txout_scriptPubKey,
                 txout.txout_value,
