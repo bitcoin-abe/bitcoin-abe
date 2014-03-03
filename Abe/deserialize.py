3eb00327492046b05e7e20cd4af95b4a8128a32c
John Tobey <John.Tobey@gmail.com>
2014-03-02 22:14:30 -0500

    Merge commit 985c6346387a0e01fdecf2e33c3ac8c8d5d4a759 from master.

--- Abe/deserialize.py
+++ Abe/deserialize.py
@@ -244,14 +244,26 @@ def script_GetOp(bytes):
     if opcode <= opcodes.OP_PUSHDATA4:
       nSize = opcode
       if opcode == opcodes.OP_PUSHDATA1:
-        nSize = ord(bytes[i])
-        i += 1
+        if i + 1 > len(bytes):
+          vch = "_INVALID_NULL"
+          i = len(bytes)
+        else:
+          nSize = ord(bytes[i])
+          i += 1
       elif opcode == opcodes.OP_PUSHDATA2:
-        (nSize,) = struct.unpack_from('<H', bytes, i)
-        i += 2
+        if i + 2 > len(bytes):
+          vch = "_INVALID_NULL"
+          i = len(bytes)
+        else:
+          (nSize,) = struct.unpack_from('<H', bytes, i)
+          i += 2
       elif opcode == opcodes.OP_PUSHDATA4:
-        (nSize,) = struct.unpack_from('<I', bytes, i)
-        i += 4
+        if i + 4 > len(bytes):
+          vch = "_INVALID_NULL"
+          i = len(bytes)
+        else:
+          (nSize,) = struct.unpack_from('<I', bytes, i)
+          i += 4
       if i+nSize > len(bytes):
         vch = "_INVALID_"+bytes[i:]
         i = len(bytes)
