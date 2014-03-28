ac82ca07de20a7bbe52c239a61f70cf7d1a05bff
Murali Kumar <murali@webchilly.in>
2014-03-28 13:58:04 +0530

    updated search form in header

--- Abe/abe.py
+++ Abe/abe.py
@@ -86,11 +86,11 @@ DEFAULT_TEMPLATE = """<!DOCTYPE html>
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
       <a class="logo navbar-brand" href="%(dotdot)s%(HOMEPAGE)s">%(APPNAME)s Blockchain Explorer</a> </div>
     <div class="collapse navbar-collapse"> 
-      <div class="search-form">
-       <form class="form-inline" action="%(dotdot)ssearch">
-            <input name="q" type="text" style="width:280px; padding:7px;" placeholder="Search" id="searchfld">
-          </form>
-      </div>
+      <form class="navbar-form navbar-right" role="search" action="%(dotdot)ssearch">
+        <div class="form-group">
+        <input name="q" type="text" class="form-control" style="width:280px; padding:7px;" placeholder="Search" id="searchfld">
+        </div>
+      </form>
     </div> 
   </div>
 </header>
