2dfc5ada2070092f23e0bcbb1c6500a7fcf3d573
Murali Kumar <geekmails@gmail.com>
2014-03-28 18:06:08 +0530

    removed text saying Short Link:
    removed text saying Short Link: in short_link function

--- Abe/abe.py
+++ Abe/abe.py
@@ -1856,7 +1856,7 @@ class Abe:
         else:
             full = base + link
 
-        return ['<p class="shortlink">Short Link: <a href="',
+        return ['<p class="shortlink"><a href="',
                 page['dotdot'], link, '">', full, '</a></p>\n']
 
     def fix_path_info(abe, env):
